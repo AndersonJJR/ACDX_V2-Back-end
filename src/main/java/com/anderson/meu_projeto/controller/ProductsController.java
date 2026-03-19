4c3ecc3436497ba10c33680ea3343f516dc55519
AndersonJJR <adasilvachavesjunior@gmail.com>
2026-03-19 15:58:20 -0300

    commit

--- src/main/java/com/anderson/meu_projeto/controller/ProductsController.java
+++ src/main/java/com/anderson/meu_projeto/controller/ProductsController.java
@@ -7,15 +7,12 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.anderson.meu_projeto.dto.DadosAtualizarProduto;
